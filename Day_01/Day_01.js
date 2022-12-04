566c870a1d168e823ddcbc169de8794afa17a46e
lukas <l.wiet@aol.de>
2022-12-05 00:42:38 +0100

    Day 01 works

---
+++ Day_01/Day_01.js
@@ -0,0 +1,20 @@
+// Advent of Code 2022 - Day 1
+const fs = require("fs");
+
+// read text file
+
+const elves = fs
+    .readFileSync("Day_01_input.txt", { encoding: "utf-8" })
+    .replace(/\r/g, "") // remove \r characters to avoid problems on windows
+    .trim() // remove starting & ending whitespace
+    .split('\n\n') // separate each elf package
+
+const calorie_max = () => {
+    const calories = elves.map((elf) => {
+        const calories = elf.split('\n').map(Number);
+        return calories.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    });
+    console.log(Math.max(...calories));
+}
+
+calorie_max();
