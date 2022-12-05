02a5514e70ab7fa80e7dfdc8a942197ce1f88778
el-we <58823641+el-we@users.noreply.github.com>
2022-12-05 18:46:51 +0100

    add comments

--- Day_01/Day_01.js
+++ Day_01/Day_01.js
@@ -1,8 +1,8 @@
 // Advent of Code 2022 - Day 1
+// needed to read text file
 const fs = require("fs");
 
 // read text file
-
 const elves = fs
     .readFileSync("Day_01_input.txt", { encoding: "utf-8" })
     .replace(/\r/g, "") // remove \r characters to avoid problems on windows
@@ -11,10 +11,10 @@ const elves = fs
 
 const calorieCalculator = (elf) =>
     elf
-        .split('\n')
-        .map(Number)
-        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        .split('\n') // separates each calorie value
+        .map(Number) // reads input as numeric values
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0); // sums up calories of one elf
 
-const elfCalories = elves.map(calorieCalculator);
+const elfCalories = elves.map(calorieCalculator); // sums up calories of all elves packages
 
-console.log(Math.max(...elfCalories));
+console.log(Math.max(...elfCalories)); // returns calories of best elf
