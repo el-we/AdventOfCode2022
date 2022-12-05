75188e63ed3ec4bfd3901fc97c4e90e948b43993
el-we <58823641+el-we@users.noreply.github.com>
2022-12-05 21:27:59 +0100

    complete day 02

--- Day_02/Day_02.js
+++ Day_02/Day_02.js
@@ -66,5 +66,64 @@ const scoreCalculator = (rounds) => rounds.map(pointsCalculator);
 const scores = scoreCalculator(rounds);
 const totalScoreCalculator = (scores) => scores.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 const totalScore = totalScoreCalculator(scores);
+//console.log(totalScore);
 
-console.log(totalScore);
+
+// Part 2 - changed rules
+/* Logic & Rules
+Add result of match and your choice
+A for Rock, B for Paper, and C for Scissors
+X for lose, Y for draw, and Z for win
+1 for Rock, 2 for Paper, and 3 for Scissors
+lose = 0
+draw = 3
+win  = 6
+*/
+
+const pointsCalculatorPart2 = (rds) => {
+    switch(rds[0]) {
+        // elf takes rock
+        case 'A':
+            // we lose + take scissors
+            if (rds[1] === 'X') {
+                return 3;
+                // draw + we take rock
+            } else if (rds[1] === 'Y') {
+                return 4;
+                // we win + take paper
+            } else {
+                return 8;
+            }
+        // elf takes paper
+        case 'B':
+            // we lose + take rock
+            if (rds[1] === 'X') {
+                return 1;
+                // draw + we take paper
+            } else if (rds[1] === 'Y') {
+                return 5;
+                // we win + take scissors
+            } else {
+                return 9;
+            }
+        // elf takes scissors
+        case 'C':
+            // we lose + take paper
+            if (rds[1] === 'X') {
+                return 2;
+                // draw + we take scissors
+            } else if (rds[1] === 'Y') {
+                return 6;
+                // we win + take rock
+            } else {
+                return 7;
+            }
+    }
+};
+
+const scoreCalculatorPart2 = (rounds) => rounds.map(pointsCalculatorPart2);
+
+const scoresPart2 = scoreCalculatorPart2(rounds);
+const totalScorePart2 = totalScoreCalculator(scoresPart2);
+
+console.log(totalScorePart2);
